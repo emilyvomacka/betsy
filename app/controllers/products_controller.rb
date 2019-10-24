51c3f9f5c806704a41212062997d5c123f17b789
brilatimer <brihiramine@gmail.com>
2019-10-23 20:08:14 -0700

    merge alice fix

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,55 +1,32 @@
-class ProductsController < ApplicationController
-  def index
+class ProductsController < ApplicationController 
+  def index
     @products = Product.all
   end
   
-  def show
-    render_404 unless @product
+  def show
+    @product = Product.find_by(id: params[:id])
   end
   
-  def create
-    @product = Product.new(product_params)
-    @product.merchant_id = @current_merchant.id
-        
-    if @product.save
-      flash[:status] = :success
-      flash[:success] = "Successfully created product #{@product.name}."
-      redirect_to product_path(@product)
-    else
-      flash[:status] = :failure
-      flash[:error] = "Unable to create #{@product.name}."
-      render :new, status: :bad_request
-    end
+  def create
+    @product = Product.new(product_params)
+    @product.merchant_id = @current_merchant.id
     
-  end
-  
-  def edit
-    require_login
-    @product = Product.find_by(id: params[:id])
-    render_404 unless @product
-  end
-  
-  def update
-    require_login
-    #check if user is authorized
-    @product.update_attributes(product_params)
-    if @product.save
-      flash[:status] = :success
-      flash[:result_text] = "Successfully updated #{@product.name}."
-      redirect_to product_path(@product)
+    if @product.save
+      flash[:status] = :success
+      flash[:success] = "Successfully created product #{@product.name}."
+      redirect_to product_path(@product)
     else
-      flash.now[:status] = :failure
-      flash.now[:result_text] = "Could not update #{@product.name}."
-      flash.now[:messages] = @product.errors.messages
-      render :edit, status: :not_found
+      flash[:status] = :failure
+      flash[:error] = "Unable to create product #{@product.name}."
+      render :new, status: :bad_request
     end
-        
   end
   
-  def create_review
-      
-  end
-  
+  # def edit
+  #   require_login
+  #   @product = Product.find_by(id: params[:id])
+  #   render_404 unless @product
+  # end
   
   def retire
     @product = Product.find_by(id: params[:id])
@@ -64,10 +41,28 @@ class ProductsController < ApplicationController
     end
   end
   
-  private
-    
-  def product_params
-    params.require(:product).permit(:name, :description, :price, :photo_URL, :stock, :merchant_id, :categories = [])
-  end
+  # def update
+  #   require_login
+  #   #check if user is authorized
+  #   @product.update_attributes(product_params)
+  #   if @product.save
+  #     flash[:status] = :success
+  #     flash[:result_text] = "Successfully updated #{@product.name}."
+  #     redirect_to product_path(@product)
+  #   else
+  #     flash.now[:status] = :failure
+  #     flash.now[:result_text] = "Could not update #{@product.name}."
+  #     flash.now[:messages] = @product.errors.messages
+  #     render :edit, status: :not_found
+  #   end
+  #       
+  # end
   
+  # def create_review
+  #     
+  # end
+  
+  # def retire
+  
+  # end
 end
