38a7b5dfdf410b32a9cf62617b0df8a2b8432ecd
Emily Vomacka <emilyvomacka@gmail.com>
2019-10-23 21:59:13 -0700

    custom methods for order model, methods and views for order controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,40 +6,52 @@ class OrdersController < ApplicationController
   def show
   end
   
-  def new
-    
-  end
-  
   def create
+    order_params = { 
+      order: {
+        email_address: nil,
+        mailing_address: nil,
+        customer_name: nil,
+        cc_number: nil, 
+        cc_expiration: nil,
+        cc_security_code: nil,
+        zip_code: nil,
+        cart_status: "pending"
+      }
+    }
     @order = Order.new(order_params)
     if @order.save # save returns true if the database insert succeeds
-      flash[:success] = "Order made successfully"
+      flash[:success] = "First item added to carb. Um, cart."
+      session[:order_id] = @order.id
       redirect_to root_path # go to the index so we can see the order in the list
       return
     else # save failed :(
-      flash.now[:failure] = "Order failed to save"
-      render :new # show the new order form view again
+      flash.now[:failure] = "Order failed to save. Sorry for the crumby UX."
+      redirect_to root_path
+    end
+  end
+
+  def edit
+    @order = Order.find_by(id: session[:order_id])
+    if @order.nil?
+      flash[:warning] = "Order was not found. Add more bread to your carb please."
+      redirect_to root_path 
       return
     end
   end
-  
   
   def update
-    if @order.update(order_params)
-      redirect_to root_path # go to the index so we can see the book in the list
+    if @order.update(order_params) 
+      #not sure how to add validations here (did customer fill in all fields?)
+      @order.status = "paid"
+      redirect_to root_path
       return
-    else # save failed :(
-      render :edit # show the new book form view again
+    else
+      render :edit 
       return
     end
   end
-  
-  
-  def edit
-  end
-  
-  
-  
+
   private
   
   def order_params
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,23 @@
 class Order < ApplicationRecord
   has_many :order_items
-end
+
+  def determine_status 
+    if self.name.nil? 
+      self.status = "pending"
+    elsif #if all fields are complete and order was made less than 24 hours ago you can still edit
+      #self.created_at > Datetime.now - 1.day 
+      self.status = "paid"
+    elsif #if all fields are complete and order was made more than 24 hours ago, no editing possible
+      self.status = "complete"
+    end 
+  end 
+
+  def total_cost
+    total_cost = 0
+    self.order_items.each do |item|
+      total_cost += (item.product.price * item.quantity)
+    end 
+    return total_cost
+  end 
+end 
+
