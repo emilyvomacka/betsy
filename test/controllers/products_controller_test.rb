bdee01ad7c095bbbb7a2932ca1f11571ef8c873a
brilatimer <brihiramine@gmail.com>
2019-10-24 15:48:59 -0700

    partial controller tests written for retire toggle

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -83,6 +83,16 @@ describe ProductsController do
     end
   end
   
+  describe "retire method" do
+    it "toggles product status from 'active' to 'retire'" do 
+      get retire_path(existing_product.id)
+      must_respond_with :redirect
+      must_redirect_to product_path(existing_product.id)
+    end
+    
+    
+  end
+  
   
 end
 
