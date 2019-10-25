cb30f9b175a647c5b280255a835d2a2bc4026e02
Alice <alicesunhi@gmail.com>
2019-10-24 19:00:10 -0700

    merchant controller index and show and tests for that

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,2 +1,20 @@
 class MerchantsController < ApplicationController
+  
+  def index
+    @merchants = Merchant.all
+  end
+  
+  def show 
+    @merchant = Merchant.find_by(id: params[:id])
+    
+    if @merchant.nil?
+      head :not_found
+      return
+    end
+  end
+  
+  
+  
+  
+  
 end
--- test/controllers/merchants_controller_test.rb
+++ test/controllers/merchants_controller_test.rb
@@ -1,7 +1,40 @@
 require "test_helper"
 
 describe MerchantsController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  let (:existing_merchant) { merchants(:macrina) }
+  
+  describe "index" do
+    it "succeeds when there are merchants" do
+      get merchants_path
+      
+      must_respond_with :success
+    end
+    
+    it "succeeds when there are no merchants" do
+      Merchant.all do |merchant|
+        merchant.destroy
+      end
+      
+      get merchants_path
+      
+      must_respond_with :success
+    end
+  end
+  
+  describe "show" do
+    it "succeeds for an existing merchant" do
+      get merchant_path(existing_merchant)
+      
+      must_respond_with :success
+    end
+    
+    it "renders 404 if the work doesn't exist" do
+      destroyed_id = existing_merchant.id
+      existing_merchant.destroy
+      
+      get merchant_path(destroyed_id)
+      must_respond_with :not_found
+    end
+  end
+  
 end
