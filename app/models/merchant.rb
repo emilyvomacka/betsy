ba6e83703049dde3eec777f9c30f19fef4aa75b8
Alice <alicesunhi@gmail.com>
2019-10-25 13:43:01 -0700

    changed username to nickname

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,12 +1,12 @@
 class Merchant < ApplicationRecord
   has_many :products 
-  validates :username, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true
   
   def self.build_from_github(auth_hash)
     merchant = Merchant.new
     merchant.uid = auth_hash[:uid]
     merchant.provider = "github"
-    merchant.name = auth_hash["info"]["name"]
+    merchant.name = auth_hash["info"]["nickname"]
     merchant.email = auth_hash["info"]["email"]
     return merchant
   end
