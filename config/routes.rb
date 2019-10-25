8589ecdc723348aaa8849e1b70737ea5e4e0105c
brilatimer <brihiramine@gmail.com>
2019-10-24 20:10:49 -0700

    login with github button has appeared on site

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   resources :orders
   resources :products, except: [:destroy]
   resources :merchants, only: [:index, :show]
-  
+
+  post "/login", to: "users#login"
+  get "/auth/github", as: "github_login"
   get '/products/retire/:id', to: 'products#retire', as: "retire"
+  get "/auth/:provider/callback", to: "users#create"
   
 end
