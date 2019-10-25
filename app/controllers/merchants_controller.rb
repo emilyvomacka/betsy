21c182c39edab1f0d5ba87d905640d43a51a7a4b
brilatimer <brihiramine@gmail.com>
2019-10-24 21:15:03 -0700

    updated routes to merchants not users

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -13,11 +13,41 @@ class MerchantsController < ApplicationController
     end
   end
   
+  def create
+    auth_hash = request.env["omniauth.auth"]
+    raise
+  end
+  
   def login
+    username = params[:username]
+    if username and merchant = Merchant.find_by(username: username)
+      session[:merchant_id] = merchant.id
+      flash[:status] = :success
+      flash[:result_text] = "Successfully logged in as existing merchant #{merchant.username}"
+    else
+      merchant = Merchant.new(username: username)
+      if merchant.save
+        session[:merchant_id] = merchant.id
+        flash[:status] = :success
+        flash[:result_text] = "Successfully created new merchant #{merchant.username} with ID #{merchant.id}"
+      else
+        flash.now[:status] = :failure
+        flash.now[:result_text] = "Could not log in"
+        flash.now[:messages] = merchant.errors.messages
+        render "login_form", status: :bad_request
+        return
+      end
+    end
+    redirect_to root_path
   end
   
   def logout
+    session[:merchant_id] = nil
+    flash[:status] = :success
+    flash[:result_text] = "Successfully logged out"
+    redirect_to root_path
   end
+  
   
   # def dashboard
   #   @merchant =  Merchant.find_by(id: params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
+  root "products#root"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :orders
   resources :products, except: [:destroy]
   resources :merchants, only: [:index, :show]
-
-  post "/login", to: "users#login"
+  
+  
+  # get "/login", to: "merchants#login_form", as: "login"
+  # post "/login", to: "merchants#login"
   get "/auth/github", as: "github_login"
   get '/products/retire/:id', to: 'products#retire', as: "retire"
-  get "/auth/:provider/callback", to: "users#create"
+  get "/auth/:provider/callback", to: "merchants#create"
   
 end
