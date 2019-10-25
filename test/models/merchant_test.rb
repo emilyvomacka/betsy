4a71abd43344e1c0783b5c6505dd1dcf0ef7f783
Alice <alicesunhi@gmail.com>
2019-10-24 19:46:01 -0700

    merchant tests

--- test/models/merchant_test.rb
+++ test/models/merchant_test.rb
@@ -1,9 +1,30 @@
 require "test_helper"
 
 describe Merchant do
-  describe "relations" do
-    let (:merchant) {merchants(:nouveau)}
+  let (:merchant) {merchants(:nouveau)}
+  
+  describe "validations" do
     
+    it "is valid with a username" do
+      expect(merchant.valid?).must_equal true
+    end
+    
+    it "is invalid without a username" do
+      merchant.username = nil
+      
+      expect(merchant.valid?).must_equal false
+      expect(merchant.errors.messages).must_include :username
+    end
+    
+    it 'is invalid without a unique username' do
+      invalid_merchant = Merchant.create(username: "Sea Wolf Bakers")
+      
+      expect(invalid_merchant.valid?).must_equal false
+      expect(invalid_merchant.errors.messages).must_include :username
+    end
+  end
+  
+  describe "relations" do
     it "can have one or many products" do
       merchant.must_respond_to :products
       merchant.products.each do |product|
