9c3fc471127c9ed6c2f4212f669d160d691dd84b
brilatimer <brihiramine@gmail.com>
2019-10-29 10:28:12 -0700

    adjusted logic in show for retire

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -131,12 +131,12 @@ describe ProductsController do
   
   describe "retire method" do
     it "toggles product status from 'active' to 'retire'" do 
-      get retire_path(existing_product.id)
+      post retire_path(existing_product.id)
       must_respond_with :redirect
       must_redirect_to product_path(existing_product.id)
     end
 
-    it ""
+
     
     
   end
