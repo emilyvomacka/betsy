f5098f4d6f9091d03dce4f99c636fef734f29c3a
Morgan Schuler <morganlschuler@gmail.com>
2019-10-28 20:33:00 -0700

    controller tests for update action order controller

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -27,6 +27,56 @@ describe OrdersController do
     end
   end
   
+  describe "update" do
+    it "re-renders edit page if quantity wanted is greater than stock" do
+      existing_order_params = {
+        order: {
+          customer_name: "Sea Wolf",
+          email_address: "seawolf@gmail.com",
+          mailing_address: "1234 Happy Town, Seattle, WA",
+          cc_number: "1111111111111111",
+          cc_expiration: "12/20",
+          cc_security_code: "123",
+          zip_code: "99999",
+          cart_status: "paid"
+        }
+      }
+      
+      
+      products(:daily_baguette).stock = 2
+      products(:daily_baguette).reload
+      
+      expect(patch order_path(existing_order.id), params: existing_order_params)
+      expect(existing_order.cart_status).must_equal "pending"
+      
+    end
+    
+    it "changes cart status to paid" do
+      existing_order_params = {
+        order: {
+          customer_name: "Sea Wolf",
+          email_address: "seawolf@gmail.com",
+          mailing_address: "1234 Happy Town, Seattle, WA",
+          cc_number: "1111111111111111",
+          cc_expiration: "12/20",
+          cc_security_code: "123",
+          zip_code: "99999",
+          cart_status: "paid"
+        }
+      }
+      
+      patch order_path(existing_order), params: existing_order_params
+      existing_order.reload      
+      
+      expect(existing_order.cart_status).must_equal "paid"
+      must_respond_with :redirect
+      must_redirect_to products_path
+    end
+    
+    it "reduces quantity in stock after order has been confirmed" do
+      
+    end
+  end
   
 end
 
